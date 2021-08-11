428e3b9b6e0711ba86133b1266a1d81f13bf6032
dylanbram354 <dylanbram354@gmail.com>
2021-08-11 09:48:10 -0400

    Adding authentication policy to WarrantyList

--- pageantry_form/warranty_form/views.py
+++ pageantry_form/warranty_form/views.py
@@ -4,10 +4,13 @@ from .serializers import WarrantySerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 class WarrantyList(APIView):
+
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         serializer = WarrantySerializer(data=request.data)
